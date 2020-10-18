82d0dfa6d192b471eb5c8c94dcdf33e7b8d472e8
MYACTIVITIES\Antonius <antonius.gress@myactivities.com>
2020-10-18 23:22:00 +0200

    correct json parsing growth token

--- docs/index.js
+++ docs/index.js
@@ -8,10 +8,11 @@ const params = {
 };
 
 client.api.getAaStateVars(params, function (err, result) {
-    document.getElementById("supply").innerHTML = (result.shares_supply / 1000000000);
-    console.log(result);
-    console.log(result.shares_supply);
+    var supply = result.shares_supply / 1000000000;
+    console.log(supply);
+    document.getElementById("supply").innerHTML = supply;
 });
+
 
 const addresses = [
     'PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU'
@@ -19,7 +20,14 @@ const addresses = [
 
 
 client.api.getBalances(addresses, function (err, result) {
-    document.getElementById("assets").innerHTML = `${result.PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU.base.stable / 1000000000} GBYTE + ${result.PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU.base.stable / 1000000000} GRD`;
+    console.log(Object.keys(result));
+    var base = result.PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU.base.stable / 1000000000;
+    var growthToken = result.PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU['YtEVK0inFAj3cQ3CPkJl5Kb8Ax+VlI/dqcOb6GQP64k='].stable / 1000000000;
+
+    var assets = JSON.stringify(result);
+    console.log(assets);
+    console.log(assets[0]);
+    document.getElementById("assets").innerHTML = `${base} GBYTE + ${growthToken} GRD`;
     console.log(result);
     console.log(result.PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU);
     console.log(result.PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU.base.stable / 1000000000);
