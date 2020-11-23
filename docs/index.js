9e8a96970e18b9daa171d1eeb09677634043c7f3
Tarmo <tarmo888@hotmail.com>
2020-11-24 00:18:40 +0200

    non-gbyte reserve asset

--- docs/index.js
+++ docs/index.js
@@ -27,94 +27,78 @@ function getAaBalances(params2) {
         var key1 = Object.keys(data[0]);
         var key2 = Object.keys(data[0][key1]);
         var t1OrInterestTokenName = $('#arb').val().split(',')[1]; // t1 or interest token name
-        if (key1[0] === "PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU" || key1[0] === "Y3NP6UMNFMA6DU2DGPJN4HB65KHKVAKR" || key1[0] === "YSGAUS4DWUORJMV3TEZXPCAEFCT4FGO3" || key1[0] === "USKWYTYD4NKBIOGPFGROG5NNTQQLISYB" || key1[0] === "6DV4SOBQ5XCC44242HTRXJMRQUXYZFSI" || key1[0] === "6KGD2SIKEIMWKUWC5E5265JKMMZHFWF2") {
+        var t1OrInterestAsset = data[1][t1OrInterestTokenName] ? data[1][t1OrInterestTokenName].asset_id : '';
+        var t1reserveAAs = [
+            'PVL22DMGM57FOYKJRPKMQBFM2BUSJLDU',
+            'Y3NP6UMNFMA6DU2DGPJN4HB65KHKVAKR',
+            'YSGAUS4DWUORJMV3TEZXPCAEFCT4FGO3',
+            'V7Y7N7HPF3BFAQOHG7R77JRGZA7CM7VA',
+            'USKWYTYD4NKBIOGPFGROG5NNTQQLISYB',
+            '6DV4SOBQ5XCC44242HTRXJMRQUXYZFSI',
+            '6KGD2SIKEIMWKUWC5E5265JKMMZHFWF2',
+        ];
+        if (t1reserveAAs.includes(key1[0])) {
             // if t1/reserve arb
-            var base_stable = data[0][key1] ? data[0][key1].base.stable / 1000000000 : 0;
-            var base_pending = data[0][key1] ? data[0][key1].base.pending / 1000000000 : 0;
-            var base_total = base_stable + base_pending;
-            var growthToken_pending = data[0][key1][key2[1]] ? data[0][key1][key2[1]].pending / 1000000000 : 0;
-            var growthToken_stable = data[0][key1][key2[1]] ? data[0][key1][key2[1]].stable / 1000000000 : 0;
-            var growthToken_total = growthToken_stable + growthToken_pending;
-            var growthTokenPrice = data[1][t1OrInterestTokenName].last_gbyte_value;
-            var totalPrice = base_total + (growthToken_total*growthTokenPrice);
-            var base_percentage = base_total / totalPrice * 100;
-            var arb_supply = data[2].shares_supply / 1000000000;
-            document.getElementById("supply").innerHTML = `${arb_supply} <br><a href="https://explorer.obyte.org/#${key1[0]}" target="_blank">(see on explorer)</a>`;
-            document.getElementById("share_value").innerHTML = `${(totalPrice / arb_supply).toFixed(2)}`;
+            var reserveTokenName = $('#arb').val().split(',')[2] || 'GBYTE';
+            var reserveAsset = reserveTokenName === 'GBYTE' ? 'base' : data[1][reserveTokenName].asset_id;
+            var reserveDecimals = $('#arb').val().split(',')[3] || 1000000000; // decimals of reserve asset
+            var reserveConfirmed = data[0][key1] ? data[0][key1][reserveAsset].stable / reserveDecimals : 0;
+            var reservePending = data[0][key1] ? data[0][key1][reserveAsset].pending / reserveDecimals : 0;
+            var reserveTotal = reserveConfirmed + reservePending;
+            var growthDecimals = data[1][t1OrInterestAsset] ? data[1][t1OrInterestAsset].decimals : 9;
+            var growthPending = data[0][key1][t1OrInterestAsset] ? data[0][key1][t1OrInterestAsset].pending / (10 ** growthDecimals) : 0;
+            var growthConfirmed = data[0][key1][t1OrInterestAsset] ? data[0][key1][t1OrInterestAsset].stable / (10 ** growthDecimals) : 0;
+            var growthTotal = growthConfirmed + growthPending;
+            var reservePrice = data[1][reserveTokenName] ? data[1][reserveTokenName].last_gbyte_value : 1;
+            var growthPrice = data[1][t1OrInterestTokenName].last_gbyte_value;
+            var totalPrice = (reserveTotal * reservePrice) + (growthTotal * growthPrice);
+            var arbSupply = data[2].shares_supply / reserveDecimals;
+            var poolPercentage = (reserveTotal * reservePrice) / ((reserveTotal * reservePrice) + (growthTotal * growthPrice)) * 100;
+
+            document.getElementById("supply").innerHTML = `${arbSupply} <br>(<a href="https://explorer.obyte.org/#${key1[0]}" target="_blank">see on explorer</a>)`;
+            document.getElementById("share_value").innerHTML = `${(totalPrice / arbSupply).toFixed(2)}`;
             document.getElementById("assets").innerHTML = `
-                ${(base_total).toFixed(2)} GBYTE (${base_percentage.toFixed(2)} %)<br>
-                + ${growthToken_total.toFixed(2)} ${t1OrInterestTokenName} (${(100 - base_percentage).toFixed(2)} %)<br>
+                ${(reserveTotal).toFixed(2)} ${reserveTokenName} (${poolPercentage.toFixed(2)} %)<br>
+                + ${growthTotal.toFixed(2)} ${t1OrInterestTokenName} (${(100 - poolPercentage).toFixed(2)} %)<br>
                 = ${totalPrice.toFixed(2)} GBYTE`;
             document.getElementById("actions").innerHTML = `
-            <a href="obyte:${key1[0]}?asset=base">
-                <button class="button is-small is-primary">add GBYTE</button>
-            </a>
-            <a href="obyte:${key1[0]}?asset=${encodeURIComponent(key2[1])}">
-                <button class="button is-small is-primary">add ${t1OrInterestTokenName}</button>
-            </a>
-            <a href="obyte:${key1[0]}?asset=${encodeURIComponent(data[2].shares_asset)}">
-                <button class="button is-small is-primary">withdraw</button>
-            </a>`;
-        } else {
+            <a href="obyte:${key1[0]}?asset=${encodeURIComponent(reserveAsset)}"><button class="button is-small is-primary">add ${reserveTokenName}</button></a>
+            <a href="obyte:${key1[0]}?asset=${encodeURIComponent(t1OrInterestAsset)}"><button class="button is-small is-primary">add ${t1OrInterestTokenName}</button></a>
+            <a href="obyte:${key1[0]}?asset=${encodeURIComponent(data[2].shares_asset)}"><button class="button is-small is-primary">withdraw both</button></a>`;
+        }
+        else {
             // if stable/interest arb
             var interestToken = $('#arb').val().split(',')[1];
-            var stableToken = $('#arb').val().split(',')[2];
-            var interestDecimals = $('#arb').val().split(',')[3]; // decimals of interest and stable token
+            var stableTokenName = $('#arb').val().split(',')[2];
+            var interestDecimals = $('#arb').val().split(',')[3]; // decimals of interest, stable and share token
             var interestTokenPrice = data[1][interestToken].last_gbyte_value;
-            var stableTokenPrice = data[1][stableToken].last_gbyte_value;
-            var arb_supply = data[2].shares_supply / interestDecimals;
-            if (!data[0][key1][key2[2]]) {
-                // case if only interest token and gbyte in assets
-                var interest_pending = data[0][key1][key2[1]] ? data[0][key1][key2[1]].pending / interestDecimals : 0;
-                var interest_stable = data[0][key1][key2[1]] ? data[0][key1][key2[1]].stable / interestDecimals : 0;
-                var interest_total = interest_pending + interest_stable;
-                var totalPrice = interestTokenPrice * interest_total;
-                document.getElementById("supply").innerHTML = `${arb_supply} <br><a href="https://explorer.obyte.org/#${key1[0]}" target="_blank">(see on explorer)</a>`;
-                document.getElementById("share_value").innerHTML = `${(totalPrice / arb_supply).toFixed(2)}`;
-                document.getElementById("assets").innerHTML = `
-                    ${interest_total.toFixed(2)} ${t1OrInterestTokenName} (100 %)<br>
-                    = ${totalPrice.toFixed(2)} GBYTE`;
-                document.getElementById("actions").innerHTML = `
-                <a href="obyte:${key1[0]}?asset=${encodeURIComponent(key2[1])}">
-                    <button class="button is-small is-primary">add ${interestToken}</button>
-                </a>
-                <a href="obyte:${key1[0]}?asset=${encodeURIComponent(data[2].shares_asset)}">
-                    <button class="button is-small is-primary">withdraw</button>
-                </a>`;
-            } else {
-                //case if interest tokens and stable tokens and arb asset in AA balance, e.g USD
-                var interest_pending = data[0][key1][key2[2]] ? data[0][key1][key2[2]].pending / interestDecimals : 0;
-                var interest_stable = data[0][key1][key2[2]] ? data[0][key1][key2[2]].stable / interestDecimals : 0;
-                var interest_total = interest_pending + interest_stable;
-                var stable_pending = data[0][key1][key2[3]] ? data[0][key1][key2[3]].pending / interestDecimals : 0;
-                var stable_stable = data[0][key1][key2[3]] ? data[0][key1][key2[3]].stable / interestDecimals : 0;
-                var stable_total = stable_pending + stable_stable;
-                var totalPrice = (interestTokenPrice * interest_total) + (stableTokenPrice * stable_total);
-                var interest_percentage = (interestTokenPrice * interest_total) / ((interestTokenPrice * interest_total) + (stableTokenPrice * stable_total)) *100;
-                document.getElementById("supply").innerHTML = `${arb_supply} <br><a href="https://explorer.obyte.org/#${key1[0]}" target="_blank">(see on explorer)</a>`;
-                document.getElementById("share_value").innerHTML = `${(totalPrice / arb_supply).toFixed(2)}`;
-                document.getElementById("assets").innerHTML = `
-                    ${interest_total.toFixed(2)} ${interestToken} (${interest_percentage.toFixed(2)} %)<br>
-                    + ${stable_total.toFixed(2)} ${stableToken} (${(100 - interest_percentage).toFixed(2)} %)<br>
-                    = ${totalPrice.toFixed(2)} GBYTE`;
-                document.getElementById("actions").innerHTML = `
-                <a href="obyte:${key1[0]}?asset=${encodeURIComponent(key2[2])}">
-                    <button class="button is-small is-primary">add ${interestToken}</button>
-                </a>
-                <a href="obyte:${key1[0]}?asset=${encodeURIComponent(data[2].shares_asset)}">
-                    <button class="button is-small is-primary">withdraw</button>
-                </a>`;
-            }
-            
+            var stableTokenPrice = data[1][stableTokenName].last_gbyte_value;
+            var stableAsset = data[1][stableTokenName].asset_id;
+            var interestPending = data[0][key1][t1OrInterestAsset] ? data[0][key1][t1OrInterestAsset].pending / interestDecimals : 0;
+            var interestConfirmed = data[0][key1][t1OrInterestAsset] ? data[0][key1][t1OrInterestAsset].stable / interestDecimals : 0;
+            var interestTotal = interestPending + interestConfirmed;
+            var stablePending = data[0][key1][stableAsset] ? data[0][key1][stableAsset].pending / interestDecimals : 0;
+            var stableConfirmed = data[0][key1][stableAsset] ? data[0][key1][stableAsset].stable / interestDecimals : 0;
+            var stableTotal = stablePending + stableConfirmed;
+            var totalPrice = (interestTokenPrice * interestTotal) + (stableTokenPrice * stableTotal);
+            var arbSupply = data[2].shares_supply / interestDecimals;
+            var poolPercentage = (interestTokenPrice * interestTotal) / ((interestTokenPrice * interestTotal) + (stableTokenPrice * stableTotal)) * 100;
+
+            document.getElementById("supply").innerHTML = `${arbSupply} <br>(<a href="https://explorer.obyte.org/#${key1[0]}" target="_blank">see on explorer</a>)`;
+            document.getElementById("share_value").innerHTML = `${(totalPrice / arbSupply).toFixed(2)}`;
+            document.getElementById("assets").innerHTML = `${interestTotal.toFixed(2)} ${interestToken} (${poolPercentage.toFixed(2)} %)<br>`;
+            document.getElementById("assets").innerHTML += stableTotal ? `+ ${stableTotal.toFixed(2)} ${stableTokenName} (${(100 - poolPercentage).toFixed(2)} %)<br>` : '';
+            document.getElementById("assets").innerHTML += `= ${totalPrice.toFixed(2)} GBYTE`;
+            document.getElementById("actions").innerHTML = `
+            <a href="obyte:${key1[0]}?asset=${encodeURIComponent(t1OrInterestAsset)}"><button class="button is-small is-primary">add ${interestToken}</button></a>
+            <a href="obyte:${key1[0]}?asset=${encodeURIComponent(data[2].shares_asset)}"><button class="button is-small is-primary">withdraw ${interestToken}</button></a>`;
         }
-        
-    })
-    
+    });
 }
 
 function update() {
-    $('#supply-arb-type').text($('#arb option:selected').text());
-    $('#supply-arb-type2').text($('#arb option:selected').text());
+    $('#supply-arb-type').text($('#arb option:selected').attr('rel'));
+    $('#supply-arb-type2').text($('#arb option:selected').attr('rel'));
     var address = $('#arb').val().split(',')[0];
     getAaBalances([address]);
 }
